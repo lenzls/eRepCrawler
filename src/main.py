42e5e6efb13a34b26f80cfd6b8c6bc833c5ce5dd
Simon Lenz <lenz.simon@googlemail.com>
2011-03-25 17:28:06 +0100

    print duration at the end
    Signed-off-by: Simon Lenz <lenz.simon@googlemail.com>

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ Created on 23.03.2011
 
 from eRepCrawler import ERepCrawler
 from eRepDBInterface import ERepDBInterface
+import time
 
 def standardCrawlTask(crawler):
     crawler.addCitizensOfBundesgebiet()
@@ -13,25 +14,24 @@ def standardCrawlTask(crawler):
     crawler.printStats()
 
 if __name__ == '__main__':
-
+    starttime = time.time()
     crawler = ERepCrawler()
+    dbInterface = ERepDBInterface()
 
-    standardCrawlTask(crawler)
+    #standardCrawlTask(crawler)
     #crawler.addCitizensOfBundesgebiet()
     #crawler.addCitizen(3051253)
     #crawler.addGeneralDataOfWorld()
     #crawler.printStats()
     
-    
+    #--------------------------------
 
     #query = """SELECT citLevel, COUNT(citLevel) AS anzahl FROM citizens WHERE citCitshipCounID == 12 GROUP BY citLevel"""
     #query = """SELECT citMilitaryRank, COUNT(citMilitaryRank) AS anzahl FROM citizens WHERE citCitshipCounID == 12 GROUP BY citMilitaryRank"""
 
-    dbInterface = ERepDBInterface()
     #dbInterface.executeQuery(query)
     #dbInterface.printCurQueryResult()
     #dbInterface.writeCurQueryResult2CSV()
-    
-    
-    
-    
+
+    endtime = time.time()
+    print "The script runned %f seconds" %(endtime-starttime)
