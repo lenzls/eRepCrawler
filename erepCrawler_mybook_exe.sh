b710c2d26ca7a5d289b7671ade3b73a404afdbcf
Simon Lenz <lenz.simon@googlemail.com>
2011-04-04 22:43:47 +0200

    new test on crontab script
    Signed-off-by: Simon Lenz <lenz.simon@googlemail.com>

--- erepCrawler_mybook_exe.sh
+++ erepCrawler_mybook_exe.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 echo "starting erep Crawler"
-python /root/eRepCrawler/src/main.py -t standard |  mutt -s "eRepCrawler Status-Bericht" lenz.simon@googlemail.com
+python /root/eRepCrawler/src/main.py -t extended |  mutt -s "eRepCrawler Status-Bericht" lenz.simon@googlemail.com
+#python /home/simon/dev/erepCrawler/src/main.py -t extended
+echo "finished crawler"
